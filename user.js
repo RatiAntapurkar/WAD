5dfa754f1bf33f50e0f4f9fbc3e495edc3184499
Rati_Antapurkar <ratiantapurkar001@gmail.com>
2025-02-14 18:36:40 +0530

    Assigmnent added

---
+++ user.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#submit").click(function() {
+        const user = {
+            name: $("#name").val(),
+            email: $("#email").val(),
+            password: $("#password").val()
+        };
+        let users = JSON.parse(localStorage.getItem("users") || "[]");
+
+        users.push(user);
+
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("User Added");
+        
+    })
+
+    $("#delete").click(function() {
+        const emailToDelete = $("#email").val(); 
+        let users = JSON.parse(localStorage.getItem("users") || "[]")
+
+        users = users.filter(user => user.email !== emailToDelete);
+
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("User Deleted");
+        
+    })
+})
---
+++ users.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    let users = JSON.parse(localStorage.getItem('users')||"[]")
+    let userList = $("#userList");
+
+    if(users.length === 0) {
+        alert("No users registered");
+        } else {
+            users.forEach(user => {
+                userList.append(`<li> name: ${user.name} Email: ${user.email} password: ${user.password}</li>`);
+            });
+
+        }
+    })
